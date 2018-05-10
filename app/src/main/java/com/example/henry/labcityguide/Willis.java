f84b679f86df00f712c4e9e39be650b1e30ec670
nhenry021 <nhenry021@yahoo.com>
2018-05-10 06:45:54 -0400

    Initial commit

---
+++ app/src/main/java/com/example/henry/labcityguide/Willis.java
@@ -0,0 +1,13 @@
+package com.example.henry.labcityguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Willis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_willis);
+    }
+}
